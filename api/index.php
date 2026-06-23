2eb8bc5b8bc179be6f4aacc162aac738bbd10ff7
anonymous21mrx <anonymous.mrx21@gmail.com>
2026-06-24 01:58:45 +0700

    Fix Vercel 500 Error: read-only filesystem storage and static routing

--- api/index.php
+++ api/index.php
@@ -1,3 +1,15 @@
 <?php
-// Meneruskan request dari Vercel ke file public/index.php bawaan Laravel
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Vercel serverless environment is read-only. We must change the storage path to /tmp
+$app->useStoragePath('/tmp/storage');
+
+$app->handleRequest(Request::capture());
